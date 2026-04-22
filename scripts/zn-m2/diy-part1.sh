0853943b83e202100b44865867b35998af8c2efb
hili68888 <99248065+hili68888@users.noreply.github.com>
2026-04-22 22:40:15 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -12,4 +12,9 @@
 
 # Uncomment a feed source
 #echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
+
+# 移除旧的 openclash (如果有的话)
+rm -rf feeds/openclash
+
+# 强制添加官方主分支
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;main' >> feeds.conf.default
